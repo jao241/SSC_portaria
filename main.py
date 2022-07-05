898fca3ab83d7c1c3813c62606371f1203488d2a
João Vitor Lima Lago Santos <vorque45@gmail.com>
2022-07-05 16:33:07 -0300

    adição de constante e print de pessoas dentro do condominio

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ LISTA_FOTOS = [
     "BD/faces/Mateus Solano.jpg"
 ]
 
+TIMEOUT_INTERVALO = 1
 
 def configuracao_inicial():
     global configuracao
@@ -35,8 +36,9 @@ def selecionar_pessoa(env):
     while True:
         foto_selecionada_aleatoriamente = random.choice(LISTA_FOTOS)
         
+        print(f'Numero da simulação: {env.now}\n')
         print(f"foto selecionada: {foto_selecionada_aleatoriamente}")
-        yield env.timeout(1)
+        yield env.timeout(TIMEOUT_INTERVALO)
 
 # Será um gerador de evento.
 def configurar_reconhecedor_face(env):
@@ -49,7 +51,7 @@ def configurar_reconhecedor_face(env):
         foto_selecionada = face_recognition.load_image_file(foto_selecionada_aleatoriamente)
         foto_original_criptografada = face_recognition.face_encodings(foto_selecionada)[0]
         
-        yield env.timeout(1)
+        yield env.timeout(TIMEOUT_INTERVALO)
     
 
 def reconhecer_face(foto):
@@ -74,7 +76,7 @@ def verifica_na_lista(env):
         for foto in LISTA_FOTOS:
             if reconhecer_face(foto):
                 pessoa_reconhecida = True
-                yield env.timeout(1)
+                yield env.timeout(TIMEOUT_INTERVALO)
             else: 
                 print('Pessoa não reconhecida com base no banco de dados atual')
 
@@ -95,19 +97,19 @@ def realizar_verificacao_entrada(env):
             
             if pessoa_dados["residente"] == True:
                 print(f"Bem vindo de volta, residente {pessoa_dados['nome']}\n")
-                lista_pessoas_condominio = pessoa_dados
+                lista_pessoas_condominio.append(pessoa_dados)
             else:
                 print("Você não é um residente do condomínio, verificando autorização de entrada!\n")
 
                 if pessoa_dados["entrada_autorizada"] == True:
                     print(f"Visitante {pessoa_dados['nome']} possui autorização, por favor entre!\n")
-                    lista_pessoas_condominio = pessoa_dados
+                    lista_pessoas_condominio.append(pessoa_dados)
                 else:
                     print(f"Visitante {pessoa_dados['nome']} não possui autorização de entrada!\n")
         else: 
             print("Nenhum resultado semelhante encontrado")
 
-        yield env.timeout(1)
+        yield env.timeout(TIMEOUT_INTERVALO)
         
 def sair_condominio():
     pass
@@ -121,7 +123,8 @@ if __name__ == "__main__":
     env.process(configurar_reconhecedor_face(env))
     env.process(verifica_na_lista(env))
     env.process(realizar_verificacao_entrada(env))
-    env.run(until=10)
+    env.run(until=20)
 
+    print(f'Pessoas dentro do condomínio: {lista_pessoas_condominio}')
 
                 
